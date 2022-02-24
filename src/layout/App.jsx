405972168dc0a96d1844a8a00f4a37e0fea57520
Horacio Gutierrez <horacio.gutierrez@educacionit.com.ar>
2022-02-24 20:15:34 -0300

    Test simple sass with BEM

--- src/layout/App.jsx
+++ src/layout/App.jsx
@@ -2,41 +2,45 @@ import { toast } from "react-toastify"
 import { createEffect, createMemo, createSignal } from "solid-js"
 
 toast.configure({
-    autoClose : 1000
+    autoClose: 1000
 })
 
 const App = () => {
 
-    const [counter,setCounter] = createSignal(0)
-    const limit = createMemo(()=>10)
+    const [counter, setCounter] = createSignal(0)
+    const limit = createMemo(() => 10)
 
     const increment = () => setCounter(counter() + 1)
 
     const decrement = () => setCounter(counter() - 1)
 
-    createEffect(()=>{
-        if(counter() == 5) {
+    createEffect(() => {
+        if (counter() == 5) {
             toast.dismiss()
             toast.info("Reaching the limit...")
         }
 
-        if(counter() > 5 && counter() < limit()) {
+        if (counter() > 5 && counter() < limit()) {
             toast.dismiss()
             toast.warn(`Remaining: ${limit() - counter()}`)
         }
 
-        if(counter() == limit()) {
+        if (counter() == limit()) {
             toast.dismiss()
             toast.error("You have reached the limit!")
         }
     })
 
     return (
-        <div>
-            <p>Contador : {counter()}</p>
-            <button onClick={increment}>sumar</button>
-            <button onClick={decrement}>restar</button>
-            
+        <div className="counter">
+            <header className="counter__header">
+                <p className="counter__headerText" >Contador Actual</p>
+                <p className="counter__headerValue">{counter()}</p>
+            </header>
+            <div className="counter__actions">
+                <button className="counter__actionsButton--sumar" onClick={increment}>sumar</button>
+                <button className="counter__actionsButton--restar" onClick={decrement}>restar</button>
+            </div>
         </div>
     )
 }
